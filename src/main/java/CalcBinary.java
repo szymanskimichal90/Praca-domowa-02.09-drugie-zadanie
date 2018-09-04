2ac68b273423caa0ca78f0d09be3d8aeebdfe95c
szymanskimichal90@gmail.com <szymanskimichal90@gmial.com>
2018-09-04 21:47:01 +0200

    Added intToBinary as Void

--- src/main/java/CalcBinary.java
+++ src/main/java/CalcBinary.java
@@ -1,2 +1,16 @@
 public class CalcBinary {
+
+
+    public void intToBinary(int insertedInt) {
+        int binary[] = new int[30];
+        int i = 0;
+        while (insertedInt > 0) {
+            binary[i++] = insertedInt % 2;
+            insertedInt = insertedInt / 2;
+        }
+        for (int i1 = i - 1; i1 >= 0; i1--) {
+            System.out.print(binary[i1]);
+        }
+    }
+    public int howManyZerosBetweenOne()
 }
