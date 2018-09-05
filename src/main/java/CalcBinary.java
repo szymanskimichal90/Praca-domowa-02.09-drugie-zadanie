01e9736357769ec7d30e6c7586af72e75d41f587
szymanskimichal90@gmail.com <szymanskimichal90@gmial.com>
2018-09-05 22:05:57 +0200

    Dziala druga metoda

--- src/main/java/CalcBinary.java
+++ src/main/java/CalcBinary.java
@@ -1,7 +1,10 @@
 public class CalcBinary {
 
+    StringBuilder binaryNumber = new StringBuilder();
 
-    public void intToBinary(int insertedInt) {
+
+    public String intToBinary(int insertedInt) {
+
         int binary[] = new int[30];
         int i = 0;
         while (insertedInt > 0) {
@@ -9,8 +12,45 @@ public class CalcBinary {
             insertedInt = insertedInt / 2;
         }
         for (int i1 = i - 1; i1 >= 0; i1--) {
-            System.out.print(binary[i1]);
+            binaryNumber.append(binary[i1]);
         }
+        return binaryNumber.toString();
     }
-    public int howManyZerosBetweenOne()
+
+
+    public int howManyZerosBetweenOne() {
+
+        int length = 0;
+        String a = binaryNumber.toString();
+        int index = a.length();
+        if (a.charAt(a.length() - 1) == '0') {
+            for (int i = 0; i < index; i++) {
+                {
+                    a = a.substring(0, index - 1);
+                    index = a.length();
+                }
+            }
+        }
+        String[] strings = a.split("1");
+        for (String string : strings) {
+
+            if (string.length() > length) {
+                length = string.length();
+            }
+        }
+        return length;
+    }
 }
+
+
+//        String[] strings = a.split("1");
+//        int length = 0;
+//        for (String string : strings) {
+//
+//            if (string.length() > length) {
+//                length = string.length();
+//            }
+//        }
+//        return length;
+//    }
+//}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        CalcBinary calc = new CalcBinary();
+        String a = calc.intToBinary(729);
+        System.out.println(a);
+        System.out.println(calc.howManyZerosBetweenOne());
 
-        new CalcBinary().intToBinary(721);
+
     }
 }
