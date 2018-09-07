f98ef80d1136ed7eb4b9a4daf7aa9160c4909d16
szymanskimichal90@gmail.com <szymanskimichal90@gmial.com>
2018-09-07 16:46:15 +0200

    Poprawiono kod. UZyto funkcji deleteChar w Stringbilder

--- src/main/java/CalcBinary.java
+++ src/main/java/CalcBinary.java
@@ -5,53 +5,26 @@ public class CalcBinary {
 
     public String intToBinary(int insertedInt) {
 
-          while (insertedInt > 0) {
+        while (insertedInt > 0) {
             number = insertedInt % 2;
             insertedInt = insertedInt / 2;
             binaryNumber.append(number);
         }
         return binaryNumber.reverse().toString();
     }
-
-
     public int howManyZerosBetweenOne() {
-
         int length = 0;
-        String a = binaryNumber.toString();
-        int index = a.length();
-        if (a.charAt(a.length() - 1) == '0') {
-            for (int i = 0; i < index; i++) {
-                {   int index1=a.length();
-                    a = a.substring(0, index1 - 1);
-
-                }
-            }
+        while (binaryNumber.charAt(binaryNumber.length() - 1) == '0') {
+            binaryNumber.deleteCharAt(binaryNumber.length() - 1);
         }
-        String[] strings = a.split("1");
+        String[] strings = binaryNumber.toString().split("1");
         for (String string : strings) {
-
             if (string.length() > length) {
                 length = string.length();
             }
         }
         return length;
     }
-
-
 }
 
 
-
-
-
-//        String[] strings = a.split("1");
-//        int length = 0;
-//        for (String string : strings) {
-//
-//            if (string.length() > length) {
-//                length = string.length();
-//            }
-//        }
-//        return length;
-//    }
-//}
